1f7469e2886c78b722e27fcfc9efdcceac834daf
HarikaV03 <hvemulapalli0313@my.msutexas.edu>
2025-02-02 15:29:26 -0600

    Added new main.cpp file and removed old files

---
+++ Bst_dlt/Bst_dlt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bst_dlt
+//
+//  Created by Harika v on 2/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
